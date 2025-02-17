6d5514552cffb96b9b6520a68b73fa527115a4dc
copep <valencope100@gmail.com>
2025-02-16 22:15:49 -0300

    cv languages

--- src/components/cv-btn.tsx
+++ src/components/cv-btn.tsx
@@ -1,18 +1,38 @@
 "use client"
 
 import { Button } from './ui/button'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from './ui/dropdown-menu'
 
 const CvBtn = () => {
-    const handleDownload = () => {
+    const handleDownload = (language : "ingles" | "español") => {
         const link = document.createElement('a')
-        link.href = '/cv-valentino.pdf' 
-        link.download = 'cv-valentino.pdf'
+        link.href = `/cv-valentino-${language}.pdf`
+        link.download = `/cv-valentino-${language}.pdf`
         link.click()
     }
     return (
-        <Button variant="outline" onClick={handleDownload}>
-            Download Cv
-        </Button>
+        <DropdownMenu>
+            <DropdownMenuTrigger>
+                <Button variant="outline">
+                    Download Cv
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuLabel>Select Language</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                    <Button variant="ghost" onClick={()=>handleDownload("ingles")}>
+                        English
+                    </Button>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                    <Button variant="ghost" onClick={()=>handleDownload("español")}>
+                        Spanish
+                    </Button>
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+
     )
 }
 
